b6b52401fc49bcd3d9c8e020dc5d2db790a7fb91
Jim Blandy <jimb@red-bean.com>
2018-06-02 14:30:03 -0700

    Make the triangle spin.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use glium::glutin::{ContextBuilder, Event, EventsLoop, WindowBuilder, WindowEven
 use glium::index::PrimitiveType;
 
 use std::error::Error;
+use std::f32::consts::PI;
+use std::time::{Instant};
 
 fn scale(a: &[f32; 3], scale: f32) -> [f32; 3] {
     [ a[0] * scale, a[1] * scale, a[2] * scale ]
@@ -23,6 +25,14 @@ fn subtract(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
     add(a, &negate(b))
 }
 
+/// Return the point `angle` radians around the origin-centered ellipse whose
+/// major axis (center to zero-radians point) is `i` and whose minor axis
+/// (center to π/2) is `j`.
+fn mix_by_angle(i: &[f32; 3], j: &[f32; 3], angle: f32) -> [f32; 3] {
+    add(&scale(i, angle.cos()),
+        &scale(j, angle.sin()))
+}
+
 /// Properties identifying windmill vane spinning about its axis of
 /// symmetry in 3-space, with a distinguished front face.
 struct Vane {
@@ -33,16 +43,32 @@ struct Vane {
     /// The midpoint of the vane's base (the side opposite the tip).
     base_midpt: [f32; 3],
 
-    /// Vector from base_midpt to the corner clockwise from the tip.
-    base_midpt_to_corner: [f32; 3]
+    /// Half the length of base - the distance from the base's midpoint to each
+    /// adjacent corner.
+    base_radius: f32,
+
+    /// Unit vector pointing from the midpoint of the base to the corner
+    /// clockwise from the tip, in the unrotated state.
+    base_unit_i: [f32; 3],
+
+    /// Unit normal to the vane, pointing outwards from the front face,
+    /// in the unrotated state.
+    base_unit_j: [f32; 3],
+
+    /// Rotation about the axis from the tip to base_midpt, in radians.
+    spin: f32
 }
 
 impl Vane {
     /// Return the positions of this vane's three corners, with the vane
     /// rotated about its axis by `spin` radians.
     fn corners(&self) -> [[f32; 3]; 3] {
-        let corner1 = add(&self.base_midpt, &self.base_midpt_to_corner);
-        let corner2 = subtract(&self.base_midpt, &self.base_midpt_to_corner);
+        let unit_towards_corner = mix_by_angle(&self.base_unit_i,
+                                               &self.base_unit_j,
+                                               self.spin);
+        let base_midpt_to_corner = scale(&unit_towards_corner, self.base_radius);
+        let corner1 = add(&self.base_midpt, &base_midpt_to_corner);
+        let corner2 = subtract(&self.base_midpt, &base_midpt_to_corner);
         // Viewed from the front, our vertices must appear in clockwise order.
         [self.tip, corner1, corner2]
     }
@@ -69,19 +95,31 @@ fn main() -> Result<(), Box<Error>> {
                              &include_str!("interior.frag"),
                              None)?;
 
-    let vane = Vane {
+    let mut vane = Vane {
         tip: [ 0.5, 0.0, 0.0 ],
         base_midpt: [ 0.0, 0.0, -0.5 ],
-        base_midpt_to_corner: [ 0.0, -0.5, 0.0 ]
+        base_radius: 0.5,
+        base_unit_i: [ 0.0, -1.0, 0.0 ],
+        base_unit_j: [ -0.707, 0.0, 0.707 ],
+        spin: 0.0
     };
+
+    let start_time = Instant::now();
 
     let mut window_open = true;
     while window_open {
+        let frame_time = Instant::now() - start_time;
+
+        let seconds = frame_time.as_secs() as f32 +
+            (frame_time.subsec_nanos() as f32 * 1e-9);
+        let spin = seconds * 0.125 * 2.0 * PI;
+
         let mut frame = display.draw();
         frame.clear_color(1.0, 1.0, 1.0, 1.0);
 
         let mut vertices = Vec::new();
 
+        vane.spin = spin;
         vertices.extend(vane.corners().iter()
                         .map(|&position| Vertex { position }));
         assert_eq!(vertices.len(), 3);
